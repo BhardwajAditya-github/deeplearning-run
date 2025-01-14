6aebfd802bd2f97606fe6add9eb62258cc43658c
Aditya Bhardwaj <74232587+BhardwajAditya-github@users.noreply.github.com>
2025-01-14 23:08:30 +0530

    Created model for multiclass classification

--- day4_multiclass_classification.py
+++ day4_multiclass_classification.py
@@ -1,7 +1,20 @@
+import sklearn.model_selection
 import torch
+import torch.nn as nn
 import sklearn.datasets as datasets
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+
+
+def data_information(X, y):
+    print("----------------x---------------")
+    print(f"X - {X.shape}")
+    print(f"Size - {X.size}")
+    print(X[:5])
+    print("----------------y---------------")
+    print(f"Y - {y.shape}")
+    print(f"Size - {y.size}")
+    print(y[:5])
 
 def visualizer(X, y):
     pca = PCA(n_components=2)
@@ -15,12 +28,34 @@ def visualizer(X, y):
     plt.colorbar()
     plt.show()
 
-X,y = datasets.make_multilabel_classification()
-print("----------------x---------------")
-print(f"X - {X.shape}")
-print(X[:5])
-print("----------------y---------------")
-print(f"Y - {y.shape}")
-print(y[:5])
+dataset_size = 1000
 
+X,y = datasets.make_multilabel_classification(n_samples=dataset_size)
+
+data_information(X, y)
 visualizer(X, y)
+
+device = "cuda" if torch.cuda.is_available else "cpu"
+
+# Split Data
+
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2, random_state=42)
+data_information(X_train, X_test)
+data_information(y_train, y_test)
+
+class MultiClassClassification(nn.Module):
+    def __init__(self):
+        super.__init__();
+        self.layer1 = nn.Linear(in_features=20, out_features=160)
+        self.layer2 = nn.Linear(in_features=160, out_features=160)
+        self.layer3 = nn.Linear(in_features=160, out_features=5)
+        self.relu = nn.ReLU()
+
+
+    def forward(self, x):
+        return self.layer3(self.relu(self.layer2(self.relu(self.layer1(x)))))
+    
+multiClassify = MultiClassClassification()
+
+loss_fcn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(multiClassify.parameters(), lr=0.01)
