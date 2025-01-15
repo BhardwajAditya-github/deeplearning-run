2d1e34f18db35deda759ba4a182bf3d91b8d7f15
Aditya Bhardwaj <74232587+BhardwajAditya-github@users.noreply.github.com>
2025-01-15 23:02:26 +0530

    Trained multi class model

--- day4_multiclass_classification.py
+++ day4_multiclass_classification.py
@@ -28,12 +28,19 @@ def visualizer(X, y):
     plt.colorbar()
     plt.show()
 
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc
+
 dataset_size = 1000
 
 X,y = datasets.make_multilabel_classification(n_samples=dataset_size)
+X = torch.from_numpy(X).type(torch.float)
+y = torch.from_numpy(y).type(torch.float)
 
-data_information(X, y)
-visualizer(X, y)
+# data_information(X, y)
+# visualizer(X, y)
 
 device = "cuda" if torch.cuda.is_available else "cpu"
 
@@ -45,7 +52,7 @@ data_information(y_train, y_test)
 
 class MultiClassClassification(nn.Module):
     def __init__(self):
-        super.__init__();
+        super().__init__();
         self.layer1 = nn.Linear(in_features=20, out_features=160)
         self.layer2 = nn.Linear(in_features=160, out_features=160)
         self.layer3 = nn.Linear(in_features=160, out_features=5)
@@ -55,7 +62,39 @@ class MultiClassClassification(nn.Module):
     def forward(self, x):
         return self.layer3(self.relu(self.layer2(self.relu(self.layer1(x)))))
     
-multiClassify = MultiClassClassification()
+MultiClassify = MultiClassClassification()
 
 loss_fcn = nn.BCEWithLogitsLoss()
-optimizer = torch.optim.SGD(multiClassify.parameters(), lr=0.01)
+optimizer = torch.optim.SGD(MultiClassify.parameters(), lr=0.01)
+
+# Model Training
+
+epochs = 100
+
+for epoch in range(epochs):
+    MultiClassify.train()
+
+    train_pred = MultiClassify(X_train)
+    train_pred_not_in_logits = torch.round(torch.sigmoid(train_pred.squeeze()))
+    train_loss = loss_fcn(y_train, train_pred)
+
+    train_acc = accuracy_fn(y_true=y_train,
+                               y_pred=train_pred_not_in_logits)
+
+    optimizer.zero_grad()
+    train_loss.backward()
+    MultiClassify.eval()
+
+    optimizer.step()
+
+    with torch.inference_mode():
+        test_pred = MultiClassify(X_test)
+        test_pred_not_in_logits = torch.round(torch.sigmoid(test_pred.squeeze()))
+        test_loss = loss_fcn(test_pred, y_test)
+
+        test_acc = accuracy_fn(y_true=y_test,
+                               y_pred=test_pred_not_in_logits)
+
+    # Print out what's happening every 10 epochs
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch} | Loss: {train_loss:.5f}, Accuracy: {train_acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
